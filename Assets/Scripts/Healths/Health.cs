f76a2fb69410563d9e40453bbba6ceb38cacbc6f
Cihat Çıtak <cihatcitakc@gmail.com>
2023-03-13 01:37:52 +0300

    Health Part 3

--- Assets/Scripts/Healths/Health.cs
+++ Assets/Scripts/Healths/Health.cs
@@ -1,45 +1,48 @@
 using UnityEngine.Events;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+namespace Healths
 {
-    public int health;
-
-    [Header("Events")]
-    public UnityEvent OnBorn;
-    public UnityEvent OnDead;
-    public UnityEvent OnTakeDamage;
-    public UnityEvent OnHealing;
-
-    private int maxHealth;
-    public int MaxHealth { get => maxHealth; }
-
-    private int currentHealth;
-    public int CurrentHealth { get => currentHealth; }
-
-    private void Start()
+    public class Health : MonoBehaviour
     {
-        maxHealth = health;
-        currentHealth = health;
+        public int health;
 
-        OnBorn?.Invoke();
-    }
+        [Header("Events")]
+        public UnityEvent OnBorn;
+        public UnityEvent OnDead;
+        public UnityEvent OnTakeDamage;
+        public UnityEvent OnHealing;
 
-    public void TakeDamage(int damage)
-    {
-        currentHealth -= damage;
+        private int maxHealth;
+        public int MaxHealth { get => maxHealth; }
 
-        OnTakeDamage?.Invoke();
+        private int currentHealth;
+        public int CurrentHealth { get => currentHealth; }
 
-        if (currentHealth <= 0)
-            OnDead?.Invoke();
-    }
+        private void Start()
+        {
+            maxHealth = health;
+            currentHealth = health;
 
-    public void Healing(int healingAmount)
-    {
-        currentHealth += healingAmount;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+            OnBorn?.Invoke();
+        }
 
-        OnHealing?.Invoke();
+        public void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+
+            OnTakeDamage?.Invoke();
+
+            if (currentHealth <= 0)
+                OnDead?.Invoke();
+        }
+
+        public void Healing(int healingAmount)
+        {
+            currentHealth += healingAmount;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+            OnHealing?.Invoke();
+        }
     }
 }
--- Assets/Scripts/Healths/HealthBar.cs
+++ Assets/Scripts/Healths/HealthBar.cs
@@ -1,18 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
+using DG.Tweening;
 
-public class HealthBar : MonoBehaviour
+namespace Healths
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HealthBar : MonoBehaviour
     {
-        
-    }
+        [SerializeField] Health health;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Fill Bar Image")]
+        [SerializeField] Image healthBarFillImage;
+
+        [Header("Health Bar Canvas Game Object")]
+        [SerializeField] GameObject canvasGameObject;
+
+        private const float FILL_DURATION = 0.25f;
+
+        public void SetHealthBarRatio()
+        {
+            if (healthBarFillImage == null)
+                return;
+
+            healthBarFillImage.DOFillAmount(CalculateHealthRatio(), FILL_DURATION);
+        }
+
+        private float CalculateHealthRatio()
+        {
+            return (float)health.CurrentHealth / (float)health.MaxHealth;
+        }
     }
 }
