61b87eca4babe109cde5398dc3a411cbfc8df6b0
Cihat Çıtak <cihatcitakc@gmail.com>
2023-04-07 19:00:51 +0300

    Animation Part 2-3

---
+++ Assets/Scripts/Character/CharacterAnimationsStrings.cs
@@ -0,0 +1,7 @@
+public class CharacterAnimationsStrings
+{
+    public const string MoveStr = "Move";
+    public const string FireStr = "Fire";
+    public const string SpeedStr = "Speed";
+    public const string DeadStr = "Dead";
+}
